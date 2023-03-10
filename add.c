70b04f78061646524624226314bdc3df5f358932
Nehchal18 <116391659+Nehchal18@users.noreply.github.com>
2023-03-10 23:07:59 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b,sum;
+    printf("Enter First number : ");
+    scanf("%d", &a); 
+    printf("Enter Second number : ");
+    scanf("%d", &b);
+    sum=a+b;
+    printf("%d + %d = %d\n",a,b,sum);
+    return 0;
+}
